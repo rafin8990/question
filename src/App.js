0d0397830827e20c42566dd38a780d6ab6848494
rafin8990 <rafin8990@gmail.com>
2022-12-23 19:37:54 +0600

    code  added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import "./App.css";
+
 
 const questions = [
   {
@@ -42,13 +44,24 @@ const questions = [
 ];
 
 function App() {
+  const [greenProgress, setGreenProgress] = useState(0)
+  const [redProgress, setRedProgress] = useState(0)
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [right, setRight] = useState(0);
+  const [wrong, setWrong] = useState(0);
+
 
   function handleAnswer(isCorrect) {
     if (isCorrect) {
+      setGreenProgress(greenProgress + 25)
       setScore(score + 1);
+      setRight(right+1)
+    }
+    else {
+      setRedProgress(redProgress + 25)
+      setWrong(wrong + 1)
     }
 
     const nextQuestion = currentQuestion + 1;
@@ -60,36 +73,58 @@ function App() {
   }
 
   return (
-    <div className="app">
-      {showScore ? (
-        <div className="score-section">
-          Você pontuou {score} de {questions.length}
+    <div>
+      <div className="d-flex around">
+        <div className="mb-3 w-100">
+          <ProgressBar now={greenProgress} label={`${greenProgress}%`} />
+          <div className="question-count text-dark">
+            <span className="text-dark"> {right}</span>/{questions.length}
+          </div>
         </div>
-      ) : (
-        <>
-          <div className="question-section">
-            <div className="question-count">
-              <span>Questão {currentQuestion + 1}</span>/{questions.length}
-            </div>
-            <div className="question-text">
-              {questions[currentQuestion].questionText}
-            </div>
+        <div className="text-center bg-dark rounded-circle mb-3 w-0 h-100 p-3 ms-3 me-3">
+          {greenProgress}
+        </div>
+        <div className="mb-3 w-100">
+          <ProgressBar variant="danger" now={redProgress} label={`${redProgress}%`} />
+          <div className="question-count text-dark">
+            <span className="text-dark">{wrong}</span>/{questions.length}
           </div>
+        </div>
+      </div>
+      <div className="app">
 
-          <div className="answer-section">
-            {questions[currentQuestion].answerOptions.map(
-              (answerOption, index) => (
-                <button
-                  onClick={() => handleAnswer(answerOption.isCorrect)}
-                  key={index}
-                >
-                  {answerOption.answerText}
-                </button>
-              )
-            )}
+        {showScore ? (
+          <div className="score-section">
+            Você pontuou {score} de {questions.length}
           </div>
-        </>
-      )}
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Questão {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+
+
+
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map(
+                (answerOption, index) => (
+                  <button
+                    onClick={() => handleAnswer(answerOption.isCorrect)}
+                    key={index}
+                  >
+                    {answerOption.answerText}
+                  </button>
+                )
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
